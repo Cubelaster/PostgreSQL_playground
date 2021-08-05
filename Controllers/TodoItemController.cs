4c1bf2c47d1cb812f4e708490c86b2d5a6e23d50
Cubelaster <david.cubela@gmail.com>
2021-08-05 11:30:12 +0200

    moving on to creating basic cruds

---
+++ Controllers/TodoItemController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Models.EfModels;
+using DataAccessLayer.Context;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PostgreSQL_Playground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly PostgreContext postgreContext;
+
+        public TodoItemController(PostgreContext postgreContext)
+        {
+            this.postgreContext = postgreContext;
+        }
+
+        // GET: api/<TodoItemController>
+        [HttpGet]
+        public List<TodoItem> Get()
+        {
+            return postgreContext.TodoItems.ToList();
+        }
+
+        // GET api/<TodoItemController>/5
+        [HttpGet("{id}")]
+        public TodoItem Get(int id)
+        {
+            return postgreContext.TodoItems.FirstOrDefault(e => e.Id == id);
+        }
+
+        // POST api/<TodoItemController>
+        [HttpPost]
+        public int Post([FromBody] TodoItem value)
+        {
+            postgreContext.TodoItems.Add(value);
+            postgreContext.SaveChanges();
+            return value.Id;
+        }
+
+        // PUT api/<TodoItemController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TodoItemController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,8 @@
+using DataAccessLayer.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +21,8 @@ namespace PostgreSQL_Playground
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PostgreContext>(
+                options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
 
